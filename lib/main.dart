32afa4810bfd08f0d19f04074783847fd37ac656
Hoàng Quốc <hoangquoc2106@gmail.com>
2024-10-14 16:32:38 +0700

    [Quốc] #2: Tích hợp jitsi_meet_flutter_sdk
    - Tích hợp jitsi_meet_flutter_sdk vào project
    - Thay main.dart bằng sample project của jiti meet trên pub.dev/packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,253 @@
 import 'package:flutter/material.dart';
 
+import 'package:jitsi_meet_flutter_sdk/jitsi_meet_flutter_sdk.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool audioMuted = true;
+  bool videoMuted = true;
+  bool screenShareOn = false;
+  List<String> participants = [];
+  final _jitsiMeetPlugin = JitsiMeet();
+
+  join() async {
+    var options = JitsiMeetConferenceOptions(
+      room: "testgabigabi",
+      configOverrides: {
+        "startWithAudioMuted": true,
+        "startWithVideoMuted": true,
+      },
+      featureFlags: {
+        FeatureFlags.addPeopleEnabled: true,
+        FeatureFlags.welcomePageEnabled: true,
+        FeatureFlags.preJoinPageEnabled: true,
+        FeatureFlags.unsafeRoomWarningEnabled: true,
+        FeatureFlags.resolution: FeatureFlagVideoResolutions.resolution720p,
+        FeatureFlags.audioFocusDisabled: true,
+        FeatureFlags.audioMuteButtonEnabled: true,
+        FeatureFlags.audioOnlyButtonEnabled: true,
+        FeatureFlags.calenderEnabled: true,
+        FeatureFlags.callIntegrationEnabled: true,
+        FeatureFlags.carModeEnabled: true,
+        FeatureFlags.closeCaptionsEnabled: true,
+        FeatureFlags.conferenceTimerEnabled: true,
+        FeatureFlags.chatEnabled: true,
+        FeatureFlags.filmstripEnabled: true,
+        FeatureFlags.fullScreenEnabled: true,
+        FeatureFlags.helpButtonEnabled: true,
+        FeatureFlags.inviteEnabled: true,
+        FeatureFlags.androidScreenSharingEnabled: true,
+        FeatureFlags.speakerStatsEnabled: true,
+        FeatureFlags.kickOutEnabled: true,
+        FeatureFlags.liveStreamingEnabled: true,
+        FeatureFlags.lobbyModeEnabled: true,
+        FeatureFlags.meetingNameEnabled: true,
+        FeatureFlags.meetingPasswordEnabled: true,
+        FeatureFlags.notificationEnabled: true,
+        FeatureFlags.overflowMenuEnabled: true,
+        FeatureFlags.pipEnabled: true,
+        FeatureFlags.pipWhileScreenSharingEnabled: true,
+        FeatureFlags.preJoinPageHideDisplayName: true,
+        FeatureFlags.raiseHandEnabled: true,
+        FeatureFlags.reactionsEnabled: true,
+        FeatureFlags.recordingEnabled: true,
+        FeatureFlags.replaceParticipant: true,
+        FeatureFlags.securityOptionEnabled: true,
+        FeatureFlags.serverUrlChangeEnabled: true,
+        FeatureFlags.settingsEnabled: true,
+        FeatureFlags.tileViewEnabled: true,
+        FeatureFlags.videoMuteEnabled: true,
+        FeatureFlags.videoShareEnabled: true,
+        FeatureFlags.toolboxEnabled: true,
+        FeatureFlags.iosRecordingEnabled: true,
+        FeatureFlags.iosScreenSharingEnabled: true,
+        FeatureFlags.toolboxAlwaysVisible: true,
+      },
+      userInfo: JitsiMeetUserInfo(
+          displayName: "Gabi",
+          email: "gabi.borlea.1@gmail.com",
+          avatar:
+              "https://avatars.githubusercontent.com/u/57035818?s=400&u=02572f10fe61bca6fc20426548f3920d53f79693&v=4"),
     );
+
+    var listener = JitsiMeetEventListener(
+      conferenceJoined: (url) {
+        debugPrint("conferenceJoined: url: $url");
+      },
+      conferenceTerminated: (url, error) {
+        debugPrint("conferenceTerminated: url: $url, error: $error");
+      },
+      conferenceWillJoin: (url) {
+        debugPrint("conferenceWillJoin: url: $url");
+      },
+      participantJoined: (email, name, role, participantId) {
+        debugPrint(
+          "participantJoined: email: $email, name: $name, role: $role, "
+          "participantId: $participantId",
+        );
+        participants.add(participantId!);
+      },
+      participantLeft: (participantId) {
+        debugPrint("participantLeft: participantId: $participantId");
+      },
+      audioMutedChanged: (muted) {
+        debugPrint("audioMutedChanged: isMuted: $muted");
+      },
+      videoMutedChanged: (muted) {
+        debugPrint("videoMutedChanged: isMuted: $muted");
+      },
+      endpointTextMessageReceived: (senderId, message) {
+        debugPrint(
+            "endpointTextMessageReceived: senderId: $senderId, message: $message");
+      },
+      screenShareToggled: (participantId, sharing) {
+        debugPrint(
+          "screenShareToggled: participantId: $participantId, "
+          "isSharing: $sharing",
+        );
+      },
+      chatMessageReceived: (senderId, message, isPrivate, timestamp) {
+        debugPrint(
+          "chatMessageReceived: senderId: $senderId, message: $message, "
+          "isPrivate: $isPrivate, timestamp: $timestamp",
+        );
+      },
+      chatToggled: (isOpen) => debugPrint("chatToggled: isOpen: $isOpen"),
+      participantsInfoRetrieved: (participantsInfo) {
+        debugPrint(
+            "participantsInfoRetrieved: participantsInfo: $participantsInfo, ");
+      },
+      readyToClose: () {
+        debugPrint("readyToClose");
+      },
+    );
+    await _jitsiMeetPlugin.join(options, listener);
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  hangUp() async {
+    await _jitsiMeetPlugin.hangUp();
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  setAudioMuted(bool? muted) async {
+    var a = await _jitsiMeetPlugin.setAudioMuted(muted!);
+    debugPrint("$a");
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      audioMuted = muted;
     });
   }
 
+  setVideoMuted(bool? muted) async {
+    var a = await _jitsiMeetPlugin.setVideoMuted(muted!);
+    debugPrint("$a");
+    setState(() {
+      videoMuted = muted;
+    });
+  }
+
+  sendEndpointTextMessage() async {
+    var a = await _jitsiMeetPlugin.sendEndpointTextMessage(message: "HEY");
+    debugPrint("$a");
+
+    for (var p in participants) {
+      var b =
+          await _jitsiMeetPlugin.sendEndpointTextMessage(to: p, message: "HEY");
+      debugPrint("$b");
+    }
+  }
+
+  toggleScreenShare(bool? enabled) async {
+    await _jitsiMeetPlugin.toggleScreenShare(enabled!);
+
+    setState(() {
+      screenShareOn = enabled;
+    });
+  }
+
+  openChat() async {
+    await _jitsiMeetPlugin.openChat();
+  }
+
+  sendChatMessage() async {
+    var a = await _jitsiMeetPlugin.sendChatMessage(message: "HEY1");
+    debugPrint("$a");
+
+    for (var p in participants) {
+      a = await _jitsiMeetPlugin.sendChatMessage(to: p, message: "HEY2");
+      debugPrint("$a");
+    }
+  }
+
+  closeChat() async {
+    await _jitsiMeetPlugin.closeChat();
+  }
+
+  retrieveParticipantsInfo() async {
+    var a = await _jitsiMeetPlugin.retrieveParticipantsInfo();
+    debugPrint("$a");
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Plugin example app'),
+          ),
+          body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: join,
+                    child: const Text("Join"),
+                  ),
+                  TextButton(onPressed: hangUp, child: const Text("Hang Up")),
+                  Row(children: [
+                    const Text("Set Audio Muted"),
+                    Checkbox(
+                      value: audioMuted,
+                      onChanged: setAudioMuted,
+                    ),
+                  ]),
+                  Row(children: [
+                    const Text("Set Video Muted"),
+                    Checkbox(
+                      value: videoMuted,
+                      onChanged: setVideoMuted,
+                    ),
+                  ]),
+                  TextButton(
+                      onPressed: sendEndpointTextMessage,
+                      child: const Text("Send Hey Endpoint Message To All")),
+                  Row(children: [
+                    const Text("Toggle Screen Share"),
+                    Checkbox(
+                      value: screenShareOn,
+                      onChanged: toggleScreenShare,
+                    ),
+                  ]),
+                  TextButton(
+                      onPressed: openChat, child: const Text("Open Chat")),
+                  TextButton(
+                      onPressed: sendChatMessage,
+                      child: const Text("Send Chat Message to All")),
+                  TextButton(
+                      onPressed: closeChat, child: const Text("Close Chat")),
+                  TextButton(
+                      onPressed: retrieveParticipantsInfo,
+                      child: const Text("Retrieve Participants Info")),
+                ]),
+          )),
     );
   }
 }
