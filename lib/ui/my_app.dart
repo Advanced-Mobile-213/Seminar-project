e3354c03fe31448adfd65e5d440112710ac1a35a
Hoàng Quốc <hoangquoc2106@gmail.com>
2024-10-15 23:16:09 +0700

    [Quốc] #4: Tách class MyApp ra file riêng, chia folder pages

---
+++ lib/ui/my_app.dart
@@ -0,0 +1,383 @@
+import 'package:flutter/material.dart';
+import 'package:jitsi_meet_flutter_sdk/jitsi_meet_flutter_sdk.dart';
+import 'package:video_conference/ui/widgets/widgets.dart';
+import 'package:gap/gap.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Optional input:
+  late String serverUrl;
+  late bool audioMuted;
+  late bool videoMuted;
+  late bool screenShareOn;
+  late List<String> participants;
+
+  // Required input:
+  late String roomName;
+  late String userDisplayName;
+  late String userAvatarURL;
+
+  @override
+  void initState() {
+    super.initState();
+    serverUrl = "https://meet.jit.si";
+    audioMuted = false;
+    videoMuted = true;
+    screenShareOn = false;
+    participants = [];
+    roomName = "";
+    userDisplayName = "";
+    userAvatarURL = "";
+  }
+
+  final _jitsiMeetPlugin = JitsiMeet();
+
+  join() async {
+    var options = JitsiMeetConferenceOptions(
+      room: roomName,
+      configOverrides: {
+        "startWithAudioMuted": audioMuted,
+        "startWithVideoMuted": videoMuted,
+      },
+      featureFlags: {
+        FeatureFlags.addPeopleEnabled: true,
+        FeatureFlags.welcomePageEnabled: true,
+        FeatureFlags.preJoinPageEnabled: true,
+        FeatureFlags.unsafeRoomWarningEnabled: true,
+        FeatureFlags.resolution: FeatureFlagVideoResolutions.resolution720p,
+        FeatureFlags.audioFocusDisabled: true,
+        FeatureFlags.audioMuteButtonEnabled: true,
+        FeatureFlags.audioOnlyButtonEnabled: true,
+        FeatureFlags.calenderEnabled: true,
+        FeatureFlags.callIntegrationEnabled: true,
+        FeatureFlags.carModeEnabled: true,
+        FeatureFlags.closeCaptionsEnabled: true,
+        FeatureFlags.conferenceTimerEnabled: true,
+        FeatureFlags.chatEnabled: true,
+        FeatureFlags.filmstripEnabled: true,
+        FeatureFlags.fullScreenEnabled: true,
+        FeatureFlags.helpButtonEnabled: true,
+        FeatureFlags.inviteEnabled: true,
+        FeatureFlags.androidScreenSharingEnabled: true,
+        FeatureFlags.speakerStatsEnabled: true,
+        FeatureFlags.kickOutEnabled: true,
+        FeatureFlags.liveStreamingEnabled: true,
+        FeatureFlags.lobbyModeEnabled: true,
+        FeatureFlags.meetingNameEnabled: true,
+        FeatureFlags.meetingPasswordEnabled: true,
+        FeatureFlags.notificationEnabled: true,
+        FeatureFlags.overflowMenuEnabled: true,
+        FeatureFlags.pipEnabled: true,
+        FeatureFlags.pipWhileScreenSharingEnabled: true,
+        FeatureFlags.preJoinPageHideDisplayName: true,
+        FeatureFlags.raiseHandEnabled: true,
+        FeatureFlags.reactionsEnabled: true,
+        FeatureFlags.recordingEnabled: true,
+        FeatureFlags.replaceParticipant: true,
+        FeatureFlags.securityOptionEnabled: true,
+        FeatureFlags.serverUrlChangeEnabled: true,
+        FeatureFlags.settingsEnabled: true,
+        FeatureFlags.tileViewEnabled: true,
+        FeatureFlags.videoMuteEnabled: true,
+        FeatureFlags.videoShareEnabled: true,
+        FeatureFlags.toolboxEnabled: true,
+        FeatureFlags.iosRecordingEnabled: true,
+        FeatureFlags.iosScreenSharingEnabled: true,
+        FeatureFlags.toolboxAlwaysVisible: true,
+      },
+      userInfo: JitsiMeetUserInfo(
+        displayName: userDisplayName,
+        //email: "gabi.borlea.1@gmail.com",
+        avatar:
+            "https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1114445501.jpg",
+      ),
+    );
+
+    var listener = JitsiMeetEventListener(
+      conferenceJoined: (url) {
+        debugPrint("conferenceJoined: url: $url");
+      },
+      conferenceTerminated: (url, error) {
+        debugPrint("conferenceTerminated: url: $url, error: $error");
+      },
+      conferenceWillJoin: (url) {
+        debugPrint("conferenceWillJoin: url: $url");
+      },
+      participantJoined: (email, name, role, participantId) {
+        debugPrint(
+          "participantJoined: email: $email, name: $name, role: $role, "
+          "participantId: $participantId",
+        );
+        participants.add(participantId!);
+      },
+      participantLeft: (participantId) {
+        debugPrint("participantLeft: participantId: $participantId");
+      },
+      audioMutedChanged: (muted) {
+        debugPrint("audioMutedChanged: isMuted: $muted");
+      },
+      videoMutedChanged: (muted) {
+        debugPrint("videoMutedChanged: isMuted: $muted");
+      },
+      endpointTextMessageReceived: (senderId, message) {
+        debugPrint(
+            "endpointTextMessageReceived: senderId: $senderId, message: $message");
+      },
+      screenShareToggled: (participantId, sharing) {
+        debugPrint(
+          "screenShareToggled: participantId: $participantId, "
+          "isSharing: $sharing",
+        );
+      },
+      chatMessageReceived: (senderId, message, isPrivate, timestamp) {
+        debugPrint(
+          "chatMessageReceived: senderId: $senderId, message: $message, "
+          "isPrivate: $isPrivate, timestamp: $timestamp",
+        );
+      },
+      chatToggled: (isOpen) => debugPrint("chatToggled: isOpen: $isOpen"),
+      participantsInfoRetrieved: (participantsInfo) {
+        debugPrint(
+            "participantsInfoRetrieved: participantsInfo: $participantsInfo, ");
+      },
+      readyToClose: () {
+        debugPrint("readyToClose");
+      },
+    );
+    await _jitsiMeetPlugin.join(options, listener);
+  }
+
+  hangUp() async {
+    await _jitsiMeetPlugin.hangUp();
+  }
+
+  setAudioMuted(bool? muted) async {
+    var a = await _jitsiMeetPlugin.setAudioMuted(muted!);
+    debugPrint("$a");
+    setState(() {
+      audioMuted = muted;
+    });
+  }
+
+  setVideoMuted(bool? muted) async {
+    var a = await _jitsiMeetPlugin.setVideoMuted(muted!);
+    debugPrint("$a");
+    setState(() {
+      videoMuted = muted;
+    });
+  }
+
+  sendEndpointTextMessage() async {
+    var a = await _jitsiMeetPlugin.sendEndpointTextMessage(message: "HEY");
+    debugPrint("$a");
+
+    for (var p in participants) {
+      var b =
+          await _jitsiMeetPlugin.sendEndpointTextMessage(to: p, message: "HEY");
+      debugPrint("$b");
+    }
+  }
+
+  toggleScreenShare(bool? enabled) async {
+    await _jitsiMeetPlugin.toggleScreenShare(enabled!);
+
+    setState(() {
+      screenShareOn = enabled;
+    });
+  }
+
+  openChat() async {
+    await _jitsiMeetPlugin.openChat();
+  }
+
+  sendChatMessage() async {
+    var a = await _jitsiMeetPlugin.sendChatMessage(message: "HEY1");
+    debugPrint("$a");
+
+    for (var p in participants) {
+      a = await _jitsiMeetPlugin.sendChatMessage(to: p, message: "HEY2");
+      debugPrint("$a");
+    }
+  }
+
+  closeChat() async {
+    await _jitsiMeetPlugin.closeChat();
+  }
+
+  retrieveParticipantsInfo() async {
+    var a = await _jitsiMeetPlugin.retrieveParticipantsInfo();
+    debugPrint("$a");
+  }
+
+  void onServerUrlChange(String value) {
+    setState(() {
+      serverUrl = value;
+    });
+  }
+
+  void onRoomNameChange(String value) {
+    setState(() {
+      roomName = value;
+    });
+  }
+
+  void onUserDisplayNameChange(String value) {
+    setState(() {
+      userDisplayName = value;
+    });
+  }
+
+  void onUserAvatarURLChange(String value) {
+    setState(() {
+      userAvatarURL = value;
+    });
+  }
+
+  void onAudioOnChange(bool value) {
+    setState(() {
+      audioMuted = value;
+    });
+  }
+
+  void onVideoOnChange(bool value) {
+    setState(() {
+      videoMuted = value;
+    });
+  }
+
+  Widget homeSampleScreen() {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
+      ),
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              TextButton(
+                onPressed: join,
+                child: const Text("Join"),
+              ),
+              TextButton(onPressed: hangUp, child: const Text("Hang Up")),
+              Row(children: [
+                const Text("Set Audio Muted"),
+                Checkbox(
+                  value: audioMuted,
+                  onChanged: setAudioMuted,
+                ),
+              ]),
+              Row(children: [
+                const Text("Set Video Muted"),
+                Checkbox(
+                  value: videoMuted,
+                  onChanged: setVideoMuted,
+                ),
+              ]),
+              TextButton(
+                  onPressed: sendEndpointTextMessage,
+                  child: const Text("Send Hey Endpoint Message To All")),
+              Row(children: [
+                const Text("Toggle Screen Share"),
+                Checkbox(
+                  value: screenShareOn,
+                  onChanged: toggleScreenShare,
+                ),
+              ]),
+              TextButton(onPressed: openChat, child: const Text("Open Chat")),
+              TextButton(
+                  onPressed: sendChatMessage,
+                  child: const Text("Send Chat Message to All")),
+              TextButton(onPressed: closeChat, child: const Text("Close Chat")),
+              TextButton(
+                  onPressed: retrieveParticipantsInfo,
+                  child: const Text("Retrieve Participants Info")),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget homeScreen() {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Video conference with Jitsi Meet'),
+      ),
+      resizeToAvoidBottomInset:
+          true, // This allows the screen to resize when the keyboard is shown
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              const Gap(20),
+              const Text('Enter your details to join the room'),
+              const Gap(20),
+              TextFieldWidget(
+                text: serverUrl,
+                onTextChange: onServerUrlChange,
+                hintText: 'Server URL',
+              ),
+              const Gap(20),
+              TextFieldWidget(
+                text: roomName,
+                onTextChange: onRoomNameChange,
+                hintText: 'Room name',
+              ),
+              const Gap(20),
+              TextFieldWidget(
+                text: userDisplayName,
+                onTextChange: onUserDisplayNameChange,
+                hintText: 'Display name',
+              ),
+              const Gap(20),
+              CheckBoxWidget(
+                icon: Icons.volume_up,
+                lable: 'Audio Muted',
+                isChecked: audioMuted,
+                onCheck: onAudioOnChange,
+              ),
+              const Gap(20),
+              CheckBoxWidget(
+                icon: Icons.videocam,
+                lable: 'Video Muted',
+                isChecked: videoMuted,
+                onCheck: (value) {
+                  setState(() {
+                    videoMuted = value;
+                  });
+                },
+              ),
+              const Gap(20),
+              ButtonWidget(
+                text: 'Join',
+                onPressed: join,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.from(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: homeScreen(),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:video_conference/main.dart';
+import 'package:video_conference/ui/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
